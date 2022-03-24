8b8db8089274437c16b3c6c5699e2194c297fb32
egorova-sofya <egorovaSjob@yandex.ru>
2022-03-24 17:27:40 +0300

    Created map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+import { useState } from "react";
+import * as parkData from "./skateboard-parks.json";
+import { Icon } from "leaflet";
+import img from "./logo192.png";
+
+const skater = new Icon({
+  iconUrl: img,
+  iconSize: [40, 40],
+});
+
+function MyComponent() {
+  const res = JSON.stringify(parkData);
+  const parkDataObj = JSON.parse(res);
+  const [activePark, setActivePark] = useState(null);
+  console.log("activePark", activePark);
+
+  const map = useMapEvents({
+    click: () => {
+      map.locate();
+    },
+  });
+
+  return (
+    <>
+      {parkDataObj.default.features.map((park) => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={[
+            park.geometry?.coordinates[1],
+            park.geometry?.coordinates[0],
+          ]}
+          eventHandlers={{
+            click: (e) => {
+              setActivePark(park);
+            },
+          }}
+          icon={skater}
+        >
+          <Popup>
+            <div>
+              <h2>{activePark?.properties.NAME}</h2>
+              <p>{activePark?.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[50.5, 30.5]} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <MyComponent />
+      </MapContainer>
     </div>
   );
 }
